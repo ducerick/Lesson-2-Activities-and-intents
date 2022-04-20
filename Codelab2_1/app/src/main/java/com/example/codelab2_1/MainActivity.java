3331c57f1dad6de9117ead296ed9c2f6eebb2ea0
Bui Minh Duc <buiminhduccsp@gmail.com>
2022-04-20 15:29:13 +0700

    Task 2: Create and launch the second Activity

--- Codelab2_1/app/src/main/java/com/example/codelab2_1/MainActivity.java
+++ Codelab2_1/app/src/main/java/com/example/codelab2_1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.codelab2_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG,"Button Clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
