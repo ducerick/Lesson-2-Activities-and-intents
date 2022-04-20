cc834e8152db7fe61c9ddbd22d86ee1fb807a78e
Bui Minh Duc <buiminhduccsp@gmail.com>
2022-04-21 00:24:08 +0700

    Task 4: Return data back to the main Activity

--- Codelab2_1/app/src/main/java/com/example/codelab2_1/MainActivity.java
+++ Codelab2_1/app/src/main/java/com/example/codelab2_1/MainActivity.java
@@ -7,10 +7,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
     private EditText mMessageEditText;
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -21,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
         mMessageEditText = findViewById(R.id.editText_main);
 
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+
     }
 
     public void launchSecondActivity(View view) {
@@ -30,6 +39,21 @@ public class MainActivity extends AppCompatActivity {
 
         String message = mMessageEditText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
--- Codelab2_1/app/src/main/java/com/example/codelab2_1/SecondActivity.java
+++ Codelab2_1/app/src/main/java/com/example/codelab2_1/SecondActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "REPLY";
+
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,21 @@ public class SecondActivity extends AppCompatActivity {
 
         textView.setText(message);
 
+        mReply = findViewById(R.id.editTextText_second);
+
+
+
+
+    }
+
+    public void returnReply(View view) {
+
+        String reply = mReply.getText().toString();
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
 
     }
 }
